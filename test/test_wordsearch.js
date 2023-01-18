c676cb7e9ef14a3aedbdf23f104bd27672ea875b
Katie Liu <katieliu59@gmail.com>
2023-01-18 17:53:26 -0500

    Implement logic to search for word vertically and return false if it is not found or array given is empty

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,46 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'G', 'H', 'U', 'E', 'Y', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['H', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['Y', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASHLEY');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'G', 'H', 'U', 'E', 'Y', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['H', 'F', 'C', 'F', 'K', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'A', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'T', 'E', 'R', 'L'],
+      ['Y', 'F', 'R', 'E', 'I', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'E', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'KATIE');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if there is an empty array", function() {
+    const result = wordSearch([], "KATIE");
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+      return false;
     }
-}
-
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    for (let i = 0; i < letters[0].length; i++) {
+        let col = letters.map(row => row[i]).join("");
+        console.log(col);
+      if (col.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
